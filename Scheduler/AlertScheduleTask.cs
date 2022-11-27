1eee986bf18d8d1d2a59f28152bfc645645e5764
Mungert69 <contact@mahadeva.co.uk>
2022-11-27 00:20:52 +0000

    ttlInSeconds 600 added

--- Scheduler/AlertScheduleTask.cs
+++ Scheduler/AlertScheduleTask.cs
@@ -18,6 +18,7 @@ namespace NetworkMonitorScheduler
         public AlertScheduleTask(DaprClient daprClient, ILogger<AlertScheduleTask> logger, IServiceScopeFactory serviceScopeFactory, IConfiguration config) : base(serviceScopeFactory)
         {
             _daprClient = daprClient;
+            _daprClient.SetMetadataAsync("ttlInSeconds","600");
             _logger = logger;
             firstRun = true;
             string scheduleStr = config.GetValue<string>("AlertSchedule");
