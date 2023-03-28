2f3490fca535997be6813de7842f6f4ff3ff1a4c
Mungert69 <contact@mahadeva.co.uk>
2023-03-29 00:29:30 +0100

    Refactored RabbitMQListener

--- Services/RabbitListener.cs
+++ Services/RabbitListener.cs
@@ -16,32 +16,15 @@ using System.Text;
 
 namespace NetworkMonitor.Objects.Repository
 {
-    public class RabbitListener
+    public class RabbitListener : RabbitListenerBase
     {
-        private string _instanceName;
-        private IModel _publishChannel;
-        private ILogger _logger;
         private IServiceState _serviceState;
-        private ConnectionFactory _factory;
-        private IConnection _connection;
-        List<RabbitMQObj> _rabbitMQObjs = new List<RabbitMQObj>();
-        public RabbitListener(ILogger logger, IServiceState serviceState,  string instanceName, string hostname)
+        public RabbitListener(ILogger logger,SystemUrl systemUrl, IServiceState serviceState) : base(logger,systemUrl)
+       
         {
-            _logger = logger;
             _serviceState=serviceState;
-            _instanceName = instanceName;
-            _factory = new ConnectionFactory
-            {
-                HostName = hostname,
-                UserName = "usercommonxf1",
-                Password = "test12",
-                VirtualHost = "/vhostuser",
-                AutomaticRecoveryEnabled = true,
-                Port = 5672
-            };
-            init();
-        }
-        public void init()
+           }
+        protected override void InitRabbitMQObjs()
         {
             _rabbitMQObjs.Add(new RabbitMQObj()
             {
@@ -70,59 +53,8 @@ namespace NetworkMonitor.Objects.Repository
                 ExchangeName = "monitorCheckServiceReady",
                 FuncName = "monitorCheckServiceReady"
             });
-            _connection = _factory.CreateConnection();
-            _publishChannel = _connection.CreateModel();
-            _rabbitMQObjs.ForEach(r => r.ConnectChannel = _connection.CreateModel());
-            var results = new List<ResultObj>();
-            results.Add(DeclareQueues());
-            results.Add(DeclareConsumers());
-            results.Add(BindChannelToConsumer());
-            bool flag = true;
-            string messages = "";
-            results.ForEach(f => messages += f.Message);
-            results.ForEach(f => flag = f.Success && flag);
-            if (flag) _logger.Info("Success : Setup RabbitListener messages were : " + messages);
-            else _logger.Fatal("Error : Failed to setup RabbitListener messages were : " + messages);
         }
-        private ResultObj DeclareQueues()
-        {
-            var result = new ResultObj();
-            result.Message = " RabbitRepo DeclareQueues : ";
-            try
-            {
-                _rabbitMQObjs.ForEach(rabbitMQObj =>
-                    {
-                        var args = new Dictionary<string, object>();
-                        if (rabbitMQObj.MessageTimeout != 0)
-                        {
-                            args.Add("x-message-ttl", rabbitMQObj.MessageTimeout);
-                        }
-                        else args = null;
-                        rabbitMQObj.QueueName = _instanceName + "-" + rabbitMQObj.ExchangeName;
-                        rabbitMQObj.ConnectChannel.ExchangeDeclare(exchange: rabbitMQObj.ExchangeName, type: ExchangeType.Fanout, durable: true);
-                        rabbitMQObj.ConnectChannel.QueueDeclare(queue: rabbitMQObj.QueueName,
-                                             durable: true,
-                                             exclusive: false,
-                                             autoDelete: true,
-                                             arguments: args
-                                             );
-                        rabbitMQObj.ConnectChannel.QueueBind(queue: rabbitMQObj.QueueName,
-                                          exchange: rabbitMQObj.ExchangeName,
-                                          routingKey: string.Empty);
-                    });
-                result.Success = true;
-                result.Message += " Success : Declared all queues ";
-            }
-            catch (Exception e)
-            {
-                string message = " Error : failed to declate queues. Error was : " + e.ToString() + " . ";
-                result.Message += message;
-                Console.WriteLine(result.Message);
-                result.Success = false;
-            }
-            return result;
-        }
-        private ResultObj DeclareConsumers()
+        protected override ResultObj DeclareConsumers()
         {
             var result = new ResultObj();
             try
@@ -186,147 +118,7 @@ namespace NetworkMonitor.Objects.Repository
             }
             return result;
         }
-        private ResultObj BindChannelToConsumer()
-        {
-            var result = new ResultObj();
-            result.Message = " RabbitRepo BindChannelToConsumer : ";
-            try
-            {
-                _rabbitMQObjs.ForEach(rabbitMQObj =>
-                    {
-                        rabbitMQObj.ConnectChannel.BasicConsume(queue: rabbitMQObj.QueueName,
-                            autoAck: false,
-                            consumer: rabbitMQObj.Consumer
-                            );
-                    });
-                result.Success = true;
-                result.Message += " Success :  bound all consumers to queues ";
-            }
-            catch (Exception e)
-            {
-                string message = " Error : failed to bind all consumers to queues. Error was : " + e.ToString() + " . ";
-                result.Message += message;
-                Console.WriteLine(result.Message);
-                result.Success = false;
-            }
-            return result;
-        }
-        private T ConvertToObject<T>(object sender, BasicDeliverEventArgs @event) where T : class
-        {
-            T result = null;
-            try
-            {
-                string json = Encoding.UTF8.GetString(@event.Body.ToArray(), 0, @event.Body.ToArray().Length);
-                var cloudEvent = JsonConvert.DeserializeObject<CloudEvent>(json);
-                JObject dataAsJObject = (JObject)cloudEvent.Data;
-                result = dataAsJObject.ToObject<T>();
-            }
-            catch (Exception e)
-            {
-                _logger.Error("Error : Unable to convert Object. Error was : " + e.ToString());
-            }
-            return result;
-        }
-
-        private string ConvertToString(object sender, BasicDeliverEventArgs @event)
-        {
-            string result = null;
-            try
-            {
-                string json = Encoding.UTF8.GetString(@event.Body.ToArray(), 0, @event.Body.ToArray().Length);
-                var cloudEvent = JsonConvert.DeserializeObject<CloudEvent>(json);
-                result = (string)cloudEvent.Data;
-            }
-            catch (Exception e)
-            {
-                _logger.Error("Error : Unable to convert Object. Error was : " + e.ToString());
-            }
-            return result;
-        }
-
-        private T ConvertToList<T>(object sender, BasicDeliverEventArgs @event) where T : class
-        {
-            T result = null;
-            try
-            {
-                string json = Encoding.UTF8.GetString(@event.Body.ToArray(), 0, @event.Body.ToArray().Length);
-                var cloudEvent = JsonConvert.DeserializeObject<CloudEvent>(json);
-                JArray dataAsJObject = (JArray)cloudEvent.Data;
-                result = dataAsJObject.ToObject<T>();
-            }
-            catch (Exception e)
-            {
-                _logger.Error("Error : Unable to convert Object. Error was : " + e.ToString());
-            }
-            return result;
-        }
-
-
-
-        public string PublishJsonZ<T>(string exchangeName, T obj) where T : class
-        {
-            var datajson = JsonUtils.writeJsonObjectToString<T>(obj);
-            string datajsonZ = StringCompressor.Compress(datajson);
-            CloudEvent cloudEvent = new CloudEvent
-            {
-                Id = "event-id",
-                Type = "event-type",
-                Source = new Uri("https://srv1.mahadeva.co.uk"),
-                Time = DateTimeOffset.UtcNow,
-                Data = datajsonZ
-            };
-            var formatter = new JsonEventFormatter();
-            var json = formatter.ConvertToJObject(cloudEvent);
-            string message = json.ToString();
-            var body = Encoding.UTF8.GetBytes(message);
-            _publishChannel.BasicPublish(exchange: exchangeName,
-                                 routingKey: string.Empty,
-                                 basicProperties: null,
-                                 // body: formatter.EncodeBinaryModeEventData(cloudEvent));
-                                 body: body);
-            return datajsonZ;
-        }
-        public void Publish<T>(string exchangeName, T obj) where T : class
-        {
-            CloudEvent cloudEvent = new CloudEvent
-            {
-                Id = "event-id",
-                Type = "event-type",
-                Source = new Uri("https://srv1.mahadeva.co.uk"),
-                Time = DateTimeOffset.UtcNow,
-                Data = obj
-            };
-            var formatter = new JsonEventFormatter();
-            var json = formatter.ConvertToJObject(cloudEvent);
-            string message = json.ToString();
-            var body = Encoding.UTF8.GetBytes(message);
-            _publishChannel.BasicPublish(exchange: exchangeName,
-                                 routingKey: string.Empty,
-                                 basicProperties: null,
-                                 // body: formatter.EncodeBinaryModeEventData(cloudEvent));
-                                 body: body);
-        }
-        public void Publish(string exchangeName, Object obj)
-        {
-            CloudEvent cloudEvent = new CloudEvent
-            {
-                Id = "event-id",
-                Type = "event-type",
-                Source = new Uri("https://srv1.mahadeva.co.uk"),
-                Time = DateTimeOffset.UtcNow,
-                Data = obj
-            };
-            var formatter = new JsonEventFormatter();
-            var json = formatter.ConvertToJObject(cloudEvent);
-            string message = json.ToString();
-            var body = Encoding.UTF8.GetBytes(message);
-            _publishChannel.BasicPublish(exchange: exchangeName,
-                                 routingKey: string.Empty,
-                                 basicProperties: null,
-                                 // body: formatter.EncodeBinaryModeEventData(cloudEvent));
-                                 body: body);
-        }
-
+   
           public ResultObj ProcessorReady(ProcessorInitObj processorObj)
         {
             ResultObj result = new ResultObj();
