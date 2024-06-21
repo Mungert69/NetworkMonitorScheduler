3cec1f9b60b1da3b185ea0372ae8bcc20bc083d4
Mungert69 <contact@mahadeva.co.ukmahadeva>
2024-06-21 08:11:04 -0700

    added check to disable payment service

--- Scheduler/PaymentScheduleTask.cs
+++ Scheduler/PaymentScheduleTask.cs
@@ -9,21 +9,29 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
 namespace NetworkMonitor.Scheduler
 {
     public class PaymentScheduleTask : ScheduledProcessor
     {
         private bool firstRun;
         private ILogger _logger;
+        private bool _disabled=false;
         public PaymentScheduleTask(ILogger<PaymentScheduleTask> logger, IServiceScopeFactory serviceScopeFactory, IConfiguration config) : base(serviceScopeFactory)
         {
             firstRun = true;
             _logger = logger;
-            string scheduleStr = config.GetValue<string>("PaymentSchedule") ?? "* * * * *";
-            updateSchedule(scheduleStr);
+            string? scheduleStr = config.GetValue<string>("PaymentSchedule") ;
+            if (scheduleStr.IsNullOrEmpty()) _disabled = true;
+            else updateSchedule(scheduleStr!);
         }
         public override Task ProcessInScope(IServiceProvider serviceProvider)
         {
+            if (_disabled) { 
+                _logger.LogInformation("SCHEDULE : Payment schedule is Disabled ");
+                return Task.CompletedTask;
+                    
+            }
             string message=" SCHEDULE : Starting Payment schedule  . ";
             IServiceState serviceState = serviceProvider.GetService<IServiceState>()!;
             //Console.WriteLine("ScheduleService : Payment Processing starts here");
